bf2e554340e6a8bdde836ed5012da83d38f89031
Caleb Klinger <Klingercaleb11@gmail.com>
2023-09-26 19:33:54 -0700

    update

---
+++ Topic 1/dq.cpp
@@ -0,0 +1,9 @@
+In support of Project 3: Carefully read the description of Project 3. 
+Choose an object from your scene and describe how you would approach rendering it.
+ Draw by hand a geometrical representation of the object and solicit feedback from the 
+ instructor and peers.
+
+I will be choosing the TV Screen to approach rendering it. I will be using the a 2D plane to the screen which will be wrapped in a texture of the relfectoin of the screen for the best look.
+Then another plane behind it which will be the rim of the TV. This will then be warped as the picture is taken at an angle bening away from the camera. Then I will design a cylander attacehd to the bottom of the planes.
+This will be the start of the stand. Where it will be attached to the final bottom rectangle. All parts besides the screen will be colored black.
+I would apoach to render the entier schene in 3D but use 2D planes for flat objects.
